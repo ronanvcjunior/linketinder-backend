c6b1dd5916ef9d42940b94733e6d608968e1b182
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2023-09-02 10:59:04 -0300

    feature: implementação do repository Candidato

---
+++ src/main/br/com/ronanjunior/linketinder/repository/CandidatoRepository.groovy
@@ -0,0 +1,7 @@
+package main.br.com.ronanjunior.linketinder.repository
+
+import main.br.com.ronanjunior.linketinder.model.Candidato
+
+class CandidatoRepository {
+    List<Candidato> candidatos;
+}
