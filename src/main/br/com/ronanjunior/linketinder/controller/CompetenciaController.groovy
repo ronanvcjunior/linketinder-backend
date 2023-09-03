501c79348837f27553928bd9f5008d8e8ed42f5b
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2023-09-03 09:15:11 -0300

    feature: implementação do contoller CompetenciaController

---
+++ src/main/br/com/ronanjunior/linketinder/controller/CompetenciaController.groovy
@@ -0,0 +1,7 @@
+package main.br.com.ronanjunior.linketinder.controller
+
+import main.br.com.ronanjunior.linketinder.model.Competencia
+
+class CompetenciaController {
+    List<Competencia> competencias;
+}
