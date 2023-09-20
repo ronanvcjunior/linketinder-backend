aeba1416d49978a4b9b0cb9a7bcdff2d00ce629d
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2023-09-20 13:55:55 -0300

    feature: footer

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -3,11 +3,15 @@ import "./assets/css/styles.css"
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
 
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 library.add(faBars);
+library.add(faGithub)
 dom.watch();
 
 new Navbar('navbar-container', true);
+new Footer('footer-container');
 
--- frontend/src/pages/perfilEmpresa.ts
+++ frontend/src/pages/perfilEmpresa.ts
@@ -5,8 +5,10 @@ import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 library.add(faBars);
 dom.watch();
 
 new Navbar('navbar-container', false);
+new Footer('footer-container');
