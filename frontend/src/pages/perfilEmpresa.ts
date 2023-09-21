b87a1827b6a0f309442be88308cfc022018dd3d2
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2023-09-21 14:49:18 -0300

    feature: perfil

--- frontend/src/pages/perfilEmpresa.ts
+++ frontend/src/pages/perfilEmpresa.ts
@@ -6,9 +6,11 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import DashboardEmpresa from "../components/DashboardEmpresa";
 
 library.add(faBars);
 dom.watch();
 
 new Navbar('navbar-container', false);
+new DashboardEmpresa("content-container");
 new Footer('footer-container');
