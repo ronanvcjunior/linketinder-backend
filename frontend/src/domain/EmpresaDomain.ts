dfcacea850570c019567880b4f0266641599e7eb
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2023-09-22 14:41:19 -0300

    feature: montando os dados no perfil da empresa

---
+++ frontend/src/domain/EmpresaDomain.ts
@@ -0,0 +1,12 @@
+interface CandidatoDomain {
+    id: number;
+    nome: string;
+    email: string;
+    cnpj: string;
+    pais: string;
+    estado: string;
+    cep: string;
+    descricao: string;
+}
+
+export default CandidatoDomain;
