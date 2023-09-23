e33967dd2ca00184597c5444cbf5b163fa6d5dfb
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2023-09-23 03:37:54 -0300

    feature: implementação do sistema de login

--- frontend/src/pages/login.ts
+++ frontend/src/pages/login.ts
@@ -1,23 +1,24 @@
 import "../assets/bootstrap/bootstrap.css"
 import "../assets/css/styles.css"
 
+
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Cadastro from "../components/Cadastro";
-import cadastroEmpresaView from "../view/cadastroEmpresaView.html";
+import loginView from "../view/loginView.html";
 
 library.add(faBars);
 dom.watch();
 
 new Navbar('navbar-container', true);
 
-const cadastroEmpresa = document.getElementById("cadastroEmpresa");
-if (cadastroEmpresa)
-    cadastroEmpresa.classList.add("ativo");
+const login = document.getElementById("login");
+if (login)
+    login.classList.add("ativo");
 
 
-new Cadastro("content-container", cadastroEmpresaView, "empresa");
+new Cadastro("content-container", loginView, "login");
 new Footer('footer-container');
