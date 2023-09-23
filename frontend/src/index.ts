90576ca332705bc71f0f1377e2db81dfd19abed2
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2023-09-23 04:59:07 -0300

    feature: implementação do sistema de redirecionamento

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,28 +1,46 @@
 import "./assets/bootstrap/bootstrap.css"
 import "./assets/css/styles.css"
 
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { dom, library } from "@fortawesome/fontawesome-svg-core";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-library.add(faBars);
-library.add(faGithub)
-dom.watch();
+declare const __webpack_public_path__: string;
 
-new Navbar("navbar-container", true);
-new Footer("footer-container");
+(() => {
+    const localStorageLogin = localStorage.getItem("login")
 
-const localStorageEmpresas: (string|null) = localStorage.getItem('empresas');
-const localStorageCandidatos: (string|null) = localStorage.getItem('candidatos');
-const localStorageVagas: (string|null) = localStorage.getItem('vagas');
-const localStorageCompetencias: (string|null) = localStorage.getItem('competencias');
+    if (localStorageLogin) {
+        const parsedLogin = JSON.parse(localStorageLogin);
+        if(parsedLogin.tipo == "empresa") {
+            window.location.href = `${__webpack_public_path__}perfilEmpresa.html`;
+            return;
+        }
+        else if (parsedLogin.tipo == "candidato") {
+            window.location.href = `${__webpack_public_path__}perfilCandidato.html`;
+            return;
+        }
+    }
 
-if (!localStorageEmpresas || !localStorageCandidatos || !localStorageVagas || !localStorageCompetencias) {
-    import("./utils/LocalStorageManager").then((module): void => {
-        const localStorageManager = module.default;
-        new localStorageManager();
-    });
-}
+    library.add(faBars);
+    library.add(faGithub);
+    dom.watch();
+
+    new Navbar("navbar-container", true);
+    new Footer("footer-container");
+    
+    const localStorageEmpresas: (string|null) = localStorage.getItem('empresas');
+    const localStorageCandidatos: (string|null) = localStorage.getItem('candidatos');
+    const localStorageVagas: (string|null) = localStorage.getItem('vagas');
+    const localStorageCompetencias: (string|null) = localStorage.getItem('competencias');
+    
+    if (!localStorageEmpresas || !localStorageCandidatos || !localStorageVagas || !localStorageCompetencias) {
+        import("./utils/LocalStorageManager").then((module): void => {
+            const localStorageManager = module.default;
+            new localStorageManager();
+        });
+    }
+})();
+
--- frontend/src/pages/cadastroCandidato.ts
+++ frontend/src/pages/cadastroCandidato.ts
@@ -4,20 +4,40 @@ import "../assets/css/styles.css"
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Cadastro from "../components/Cadastro";
 import cadastroCandidatoView from "../view/cadastroCandidadoView.html";
 
-library.add(faBars);
-dom.watch();
+declare const __webpack_public_path__: string;
 
-new Navbar('navbar-container', true);
+(() => {
+    const localStorageLogin = localStorage.getItem("login")
 
-const cadastroCandidato = document.getElementById("cadastroCandidato");
-if (cadastroCandidato)
-    cadastroCandidato.classList.add("ativo");
+    if (localStorageLogin) {
+        const parsedLogin = JSON.parse(localStorageLogin);
+        if(parsedLogin.tipo == "empresa") {
+            window.location.href = `${__webpack_public_path__}perfilEmpresa.html`;
+            return;
+        }
+        else if (parsedLogin.tipo == "candidato") {
+            window.location.href = `${__webpack_public_path__}perfilCandidato.html`;
+            return;
+        }
+    }
 
-new Cadastro("content-container", cadastroCandidatoView, "candidato");
-new Footer('footer-container');
+    library.add(faBars);
+    library.add(faGithub);
+    dom.watch();
+
+    new Navbar('navbar-container', true);
+
+    const cadastroCandidato = document.getElementById("cadastroCandidato");
+    if (cadastroCandidato)
+        cadastroCandidato.classList.add("ativo");
+
+    new Cadastro("content-container", cadastroCandidatoView, "candidato");
+    new Footer('footer-container');
+})();
