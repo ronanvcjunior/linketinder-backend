dd5a33a7d3f8e2c41d282722b1d3231ace8d14e6
Ronan Vieira do Carmo Juior <ronanvcjunior@gmail.com>
2023-11-07 21:55:48 -0300

    feature: CandidatoService

---
+++ src/main/groovy/br/com/ronanjunior/linketinder/service/CandidatoService.groovy
@@ -0,0 +1,188 @@
+package br.com.ronanjunior.linketinder.service
+
+import br.com.ronanjunior.linketinder.dao.CandidatoDao
+import br.com.ronanjunior.linketinder.dto.CandidatoListaDaEmpresaDto
+import br.com.ronanjunior.linketinder.model.Candidato
+import br.com.ronanjunior.linketinder.model.Competencia
+import br.com.ronanjunior.linketinder.model.Empresa
+import br.com.ronanjunior.linketinder.utils.Conexao
+import br.com.ronanjunior.linketinder.utils.MapperUtils
+
+import java.time.LocalDate
+
+class CandidatoService {
+    private final Conexao conexao
+    private final MapperUtils mapperUtils
+    private final CandidatoDao candidatoDao
+    private final CandidatoCompetenciaService candidatoCompetenciaService
+
+    CandidatoService() {
+        this.conexao = new Conexao()
+        this.mapperUtils = new MapperUtils()
+        this.candidatoDao = new CandidatoDao(conexao, mapperUtils)
+        this.candidatoCompetenciaService = new CandidatoCompetenciaService()
+    }
+
+    CandidatoService(Conexao conexao, MapperUtils mapperUtils, CandidatoDao candidatoDao, CandidatoCompetenciaService candidatoCompetenciaService) {
+        this.conexao = conexao
+        this.mapperUtils = mapperUtils
+        this.candidatoDao = candidatoDao
+        this.candidatoCompetenciaService = candidatoCompetenciaService
+    }
+
+    List<CandidatoListaDaEmpresaDto> listarCandidatosParaEmpresa(Empresa empresa) {
+        try {
+            conexao.abrirConexao()
+            return this.montarListaCandidatosParaEmpresa(empresa.id)
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao listar de candidatos para empresa: ${e.message}", e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    Boolean alterarCandidato(Candidato candidato) {
+        try {
+            conexao.abrirConexao()
+            return this.montarAlterarCandidato(candidato)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    Candidato buscarCandidatoPorCpf(Candidato candidato) {
+        try {
+            conexao.abrirConexao()
+            return this.montarBuscarCandidatoPorCpf(candidato.cpf)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    Candidato buscarCandidatoPorId(Candidato candidato) {
+        try {
+            conexao.abrirConexao()
+            return this.montarBuscarCandidatoPorId(candidato.id)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    protected Candidato montarBuscarCandidatoPorId(Integer idCandidato) {
+        try {
+            Map candidatoMap = candidatoDao.buscarCandidatoPorId(idCandidato)
+            List<Competencia> competencias = candidatoCompetenciaService
+                    .montarListaCompetenciaParaCandidato(candidatoMap.get("id_candidato") as Integer)
+            return new Candidato(
+                    candidatoMap.get("id_candidato") as Integer,
+                    candidatoMap.get("nome") as String,
+                    candidatoMap.get("sobrenome") as String,
+                    candidatoMap.get("cpf") as String,
+                    LocalDate.parse(candidatoMap.get("data_nascimento") as String),
+                    candidatoMap.get("pais") as String,
+                    candidatoMap.get("estado") as String,
+                    candidatoMap.get("cep") as String,
+                    candidatoMap.get("descricao") as String,
+                    competencias
+            )
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao buscar os dados do candidato por id: ${e.message}", e)
+        }
+    }
+
+    Boolean excluirCandidato(Candidato candidato) {
+        try {
+            conexao.abrirConexao()
+            return this.montarExcluirCandidato(candidato.id)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    Candidato inserirCandidato(Candidato candidato) {
+        try {
+            conexao.abrirConexao()
+            return this.montarInserirCandidato(candidato)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    protected Candidato montarInserirCandidato(Candidato candidato) {
+        try {
+            Integer idCandidato = candidatoDao.inserirCandidato(candidato)
+            this.montarBuscarCandidatoPorId(idCandidato)
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao inserir um novo candidato: ${e.message}", e)
+        }
+    }
+
+    protected Boolean montarExcluirCandidato(Integer idCandidato) {
+        try {
+            return candidatoDao.excluirCandidato(idCandidato)
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao excluir candidato: ${e.message}", e)
+        }
+    }
+
+    protected Candidato montarBuscarCandidatoPorCpf(String cpf) {
+        try {
+            Map candidatoMap = candidatoDao.buscarCandidatoPorCpf(cpf)
+            List<Competencia> competencias = candidatoCompetenciaService
+                                    .montarListaCompetenciaParaCandidato(candidatoMap.get("id_candidato") as Integer)
+            return new Candidato(
+                    candidatoMap.get("id_candidato") as Integer,
+                    candidatoMap.get("nome") as String,
+                    candidatoMap.get("sobrenome") as String,
+                    candidatoMap.get("cpf") as String,
+                    LocalDate.parse(candidatoMap.get("data_nascimento") as String),
+                    candidatoMap.get("pais") as String,
+                    candidatoMap.get("estado") as String,
+                    candidatoMap.get("cep") as String,
+                    candidatoMap.get("descricao") as String,
+                    competencias
+            )
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao buscar os dados do candidato por cpf: ${e.message}", e)
+        }
+    }
+
+    protected Boolean montarAlterarCandidato(Candidato candidato) {
+        try {
+            return candidatoDao.atualizarCandidato(candidato)
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao alterar os dados do candidato: ${e.message}", e)
+        }
+    }
+
+    protected List<CandidatoListaDaEmpresaDto> montarListaCandidatosParaEmpresa(Integer idEmpresa) {
+        try {
+            List<CandidatoListaDaEmpresaDto> candidatosListaDaEmpresaDto = []
+
+            List<Map> candidatosParaEmpresaEmMap = candidatoDao.listarCandidatosParaEmpresa(idEmpresa)
+            candidatosParaEmpresaEmMap.forEach { Map candidatoMap ->
+                List<Competencia> competencias = candidatoCompetenciaService
+                        .montarListaCompetenciaParaCandidato(candidatoMap.get("id_candidato") as Integer)
+                candidatosListaDaEmpresaDto.push(new CandidatoListaDaEmpresaDto(
+                        candidatoMap.get("id_candidato") as Integer,
+                        candidatoMap.get("nome_completo") as String,
+                        competencias
+
+                ))
+            }
+            return candidatosListaDaEmpresaDto
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao montar lista de candidatos para empresa: ${e.message}", e)
+        }
+    }
+}
---
+++ src/test/groovy/br/com/ronanjunior/linketinder/service/CandidatoServiceTest.groovy
@@ -0,0 +1,167 @@
+package br.com.ronanjunior.linketinder.service
+
+import br.com.ronanjunior.linketinder.dao.CandidatoDao
+import br.com.ronanjunior.linketinder.dto.CandidatoListaDaEmpresaDto
+import br.com.ronanjunior.linketinder.model.Candidato
+import br.com.ronanjunior.linketinder.model.Empresa
+import br.com.ronanjunior.linketinder.utils.Conexao
+import br.com.ronanjunior.linketinder.utils.MapperUtils
+import groovy.test.GroovyTestCase
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+
+import java.time.LocalDate
+
+import static org.junit.jupiter.api.Assertions.*
+
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class CandidatoServiceTest extends GroovyTestCase {
+    @InjectMocks
+    private CandidatoService candidatoService
+
+    @Mock
+    private Conexao conexao
+
+    @Mock
+    private CandidatoDao candidatoDao
+
+    @Mock
+    private CandidatoCompetenciaService candidatoCompetenciaService
+
+    @Mock
+    private MapperUtils mapperUtils
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    void testListarCandidatosParaEmpresa() {
+        List<CandidatoListaDaEmpresaDto> retornoEsperado = [
+                new CandidatoListaDaEmpresaDto(1, "Anônimo", []),
+                new CandidatoListaDaEmpresaDto(2, "Ana Julia", [])
+        ]
+        List<Map> candidatoMap = [
+                [id_candidato: 1, nome_completo: "Anônimo", match: 0],
+                [id_candidato: 2, nome_completo: "Ana Julia", match: 1]
+        ]
+        Empresa empresa = new Empresa(1, "Empresa", "012345678901234", "Brasil", "12345678", "")
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(candidatoDao.listarCandidatosParaEmpresa(Mockito.any(Integer))).thenReturn(candidatoMap)
+        Mockito.when(candidatoCompetenciaService.montarListaCompetenciaParaCandidato(Mockito.any(Integer))).thenReturn([])
+
+        List<CandidatoListaDaEmpresaDto> retorno = candidatoService.listarCandidatosParaEmpresa(empresa)
+
+        assertEquals(retornoEsperado.sort(), retorno.sort())
+    }
+
+    @Test
+    void testAlterarCandidato() {
+        Candidato candidato = new Candidato(1, "Candi", "Dato", "01234567890", LocalDate.of(1970, 1, 1), "Brasil", "GO", "12345678", "", [])
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(candidatoDao.atualizarCandidato(Mockito.any(Candidato.class))).thenReturn(true)
+
+        Boolean atualizado = candidatoService.alterarCandidato(candidato)
+
+        assertTrue(atualizado)
+    }
+
+    @Test
+    void testBuscarCandidatoPorCpf() {
+        Candidato candidatoEsperado = new Candidato(1, "Candi", "Dato", "01234567890", LocalDate.of(1970, 1, 1), "Brasil", "GO", "12345678", "", [])
+        Map candidatoMap = [
+                id_candidato: 1,
+                nome: "Candi",
+                sobrenome: "Dato",
+                cpf: "01234567890",
+                data_nascimento: "1970-01-01",
+                pais: "Brasil",
+                estado: "GO",
+                cep: "12345678",
+                descricao: ""
+        ]
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(candidatoDao.buscarCandidatoPorCpf(Mockito.any(String.class))).thenReturn(candidatoMap)
+        Mockito.when(candidatoCompetenciaService.montarListaCompetenciaParaCandidato(Mockito.any(Integer))).thenReturn([])
+
+        Candidato candidato = candidatoService.buscarCandidatoPorCpf(candidatoEsperado)
+
+        assertEquals(candidatoEsperado, candidato)
+    }
+
+    @Test
+    void testBuscarCandidatoPorId() {
+        Candidato candidatoEsperado = new Candidato(1, "Candi", "Dato", "01234567890", LocalDate.of(1970, 1, 1), "Brasil", "GO", "12345678", "", [])
+        Map candidatoMap = [
+                id_candidato: 1,
+                nome: "Candi",
+                sobrenome: "Dato",
+                cpf: "01234567890",
+                data_nascimento: "1970-01-01",
+                pais: "Brasil",
+                estado: "GO",
+                cep: "12345678",
+                descricao: ""
+        ]
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(candidatoDao.buscarCandidatoPorId(Mockito.any(Integer.class))).thenReturn(candidatoMap)
+        Mockito.when(candidatoCompetenciaService.montarListaCompetenciaParaCandidato(Mockito.any(Integer))).thenReturn([])
+
+        Candidato candidato = candidatoService.buscarCandidatoPorId(candidatoEsperado)
+
+        assertEquals(candidatoEsperado, candidato)
+    }
+
+    @Test
+    void testExcluirCandidato() {
+        Candidato candidato = new Candidato(1, "Candi", "Dato", "01234567890", LocalDate.of(1970, 1, 1), "Brasil", "GO", "12345678", "", [])
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(candidatoDao.excluirCandidato(Mockito.any(Integer.class))).thenReturn(true)
+
+        Boolean excluido = candidatoService.excluirCandidato(candidato)
+
+        assertTrue(excluido)
+    }
+
+    @Test
+    void testInserirCandidato() {
+        Candidato candidatoEsperado = new Candidato(1, "Candi", "Dato", "01234567890", LocalDate.of(1970, 1, 1), "Brasil", "GO", "12345678", "", [])
+        Map candidatoMap = [
+                id_candidato: 1,
+                nome: "Candi",
+                sobrenome: "Dato",
+                cpf: "01234567890",
+                data_nascimento: "1970-01-01",
+                pais: "Brasil",
+                estado: "GO",
+                cep: "12345678",
+                descricao: ""
+        ]
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(candidatoDao.inserirCandidato(Mockito.any(Candidato.class))).thenReturn(1)
+        Mockito.when(candidatoDao.buscarCandidatoPorId(Mockito.any(Integer.class))).thenReturn(candidatoMap)
+
+        Candidato candidato = candidatoService.inserirCandidato(candidatoEsperado)
+
+        assertEquals(candidatoEsperado, candidato)
+    }
+}
