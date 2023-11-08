9597dea823e95e2d8578aa743d5090e15e05ebca
Ronan Vieira do Carmo Juior <ronanvcjunior@gmail.com>
2023-11-08 16:28:05 -0300

    feature: CandidatoCompetenciaService

---
+++ src/main/groovy/br/com/ronanjunior/linketinder/service/CandidatoCompetenciaService.groovy
@@ -0,0 +1,112 @@
+package br.com.ronanjunior.linketinder.service
+
+import br.com.ronanjunior.linketinder.dao.CandidatoCompetenciaDao
+import br.com.ronanjunior.linketinder.dao.CandidatoCompetenciaDao
+import br.com.ronanjunior.linketinder.dto.VagaListaDoCandidatoDto
+import br.com.ronanjunior.linketinder.model.Candidato
+import br.com.ronanjunior.linketinder.model.Competencia
+import br.com.ronanjunior.linketinder.model.Vaga
+import br.com.ronanjunior.linketinder.utils.Conexao
+import br.com.ronanjunior.linketinder.utils.MapperUtils
+
+class CandidatoCompetenciaService {
+    private final Conexao conexao
+    private final MapperUtils mapperUtils
+    private final CandidatoCompetenciaDao candidatoCompetenciaDao
+
+    CandidatoCompetenciaService() {
+        this.conexao = new Conexao()
+        this.mapperUtils = new MapperUtils()
+        this.candidatoCompetenciaDao = new CandidatoCompetenciaDao(conexao, mapperUtils)
+    }
+
+    CandidatoCompetenciaService(Conexao conexao, MapperUtils mapperUtils, CandidatoCompetenciaDao candidatoCompetenciaDao) {
+        this.conexao = conexao
+        this.mapperUtils = mapperUtils
+        this.candidatoCompetenciaDao = candidatoCompetenciaDao
+    }
+
+    Competencia buscarCompetenciaDoCandidato(Candidato candidato, Competencia competencia) {
+        try {
+            conexao.abrirConexao()
+            return this.montarBuscarCompetenciaDoCandidato(candidato.id, competencia.id)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    List<Competencia> listarCompetenciasDoCandidato(Candidato candidato) {
+        try {
+            conexao.abrirConexao()
+            return this.montarListaCompetenciaParaCandidato(candidato.id)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    Boolean inserirCompetenciaParaCandidato(Candidato candidato, Competencia competencia) {
+        try {
+            conexao.abrirConexao()
+            return this.montarInserirCompeteciaParaCandidato(candidato.id, competencia.id)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    Boolean excluirCompetenciaDoCandidato(Candidato candidato, Competencia competencia) {
+        try {
+            conexao.abrirConexao()
+            return this.montarExcluirCompeteciaDoCandidato(candidato.id, competencia.id)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    protected Competencia montarBuscarCompetenciaDoCandidato(Integer idCandidato, Integer idCompetencia) {
+        try {
+            Map competenciaMap = candidatoCompetenciaDao.buscarCompetenciaCandidato(idCandidato, idCompetencia)
+
+            return new Competencia(competenciaMap)
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao montar busca de competência do candidado: ${e.message}", e)
+        }
+    }
+
+    protected List<Competencia> montarListaCompetenciaParaCandidato(Integer idCandidato) {
+        try {
+            List<Competencia> competencias = []
+            List<Map> competenciasDoCandidato = candidatoCompetenciaDao.listarCompetenciasPorCandidatoID(idCandidato)
+            competenciasDoCandidato.forEach { Map competenciaMap ->
+                competencias.push(new Competencia(competenciaMap))
+            }
+
+            return competencias;
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao montar lista de competências para candidado: ${e.message}", e)
+        }
+    }
+
+    protected Boolean montarInserirCompeteciaParaCandidato(Integer idCandidato, Integer idCompetencia) {
+        try {
+            return candidatoCompetenciaDao.cadastrarCompetenciaCandidato(idCandidato, idCompetencia)
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao inserir um nova competência para o candidato: ${e.message}", e)
+        }
+    }
+
+    protected Boolean montarExcluirCompeteciaDoCandidato(Integer idCandidato, Integer idCompetencia) {
+        try {
+            return candidatoCompetenciaDao.excluirCompetenciaCandidato(idCandidato, idCompetencia)
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao excluir a competência do candidato: ${e.message}", e)
+        }
+    }
+}
---
+++ src/test/groovy/br/com/ronanjunior/linketinder/service/CandidatoCompetenciaServiceTest.groovy
@@ -0,0 +1,92 @@
+package br.com.ronanjunior.linketinder.service
+
+import br.com.ronanjunior.linketinder.dao.CandidatoCompetenciaDao
+import br.com.ronanjunior.linketinder.model.Candidato
+import br.com.ronanjunior.linketinder.model.Competencia
+import br.com.ronanjunior.linketinder.model.Vaga
+import br.com.ronanjunior.linketinder.utils.Conexao
+import br.com.ronanjunior.linketinder.utils.MapperUtils
+import groovy.test.GroovyTestCase
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class CandidatoCompetenciaServiceTest extends GroovyTestCase {
+    @InjectMocks
+    private CandidatoCompetenciaService candidatoCompetenciaService
+
+    @Mock
+    private Conexao conexao
+
+    @Mock
+    private CandidatoCompetenciaDao candidatoCompetenciaDao
+
+    @Mock
+    private MapperUtils mapperUtils
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    void testBuscarCandidatoCompetenciaPorId() {
+        Competencia retornoEsperado = new Competencia(1, "Java")
+        Candidato candidato = new Candidato(1, null, null, null, null, null, null, null, null, null)
+        Map competenciaMap = [id_competencia: 1, nome: "Java"]
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(candidatoCompetenciaDao.buscarCompetenciaCandidato(Mockito.any(Integer.class), Mockito.any(Integer.class))).thenReturn(competenciaMap)
+
+        Competencia retorno = candidatoCompetenciaService.buscarCompetenciaDoCandidato(candidato, retornoEsperado)
+
+        assertEquals(retornoEsperado, retorno)
+    }
+
+    @Test
+    void testListarCompetenciasDoCandidato() {
+        List<Competencia> retornoEsperado = [new Competencia(1, "Java"), new Competencia(2, "Groovy")]
+        Candidato candidato = new Candidato(1, null, null, null, null, null, null, null, null, null)
+        List<Map> competenciasMap = [[id_competencia: 1, nome: "Java"], [id_competencia: 2, nome: "Groovy"]]
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(candidatoCompetenciaDao.listarCompetenciasPorCandidatoID(Mockito.any(Integer.class))).thenReturn(competenciasMap)
+
+        List<Competencia> retorno = candidatoCompetenciaService.listarCompetenciasDoCandidato(candidato)
+
+        assertEquals(retornoEsperado.sort(), retorno.sort())
+    }
+
+    @Test
+    void testInserirCompetenciaParaCandidato() {
+        Competencia competencia = new Competencia(1, "Java")
+        Candidato candidato = new Candidato(1, null, null, null, null, null, null, null, null, null)
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(candidatoCompetenciaDao.cadastrarCompetenciaCandidato(Mockito.any(Integer.class), Mockito.any(Integer.class))).thenReturn(true)
+
+        Boolean inserido = candidatoCompetenciaService.inserirCompetenciaParaCandidato(candidato, competencia)
+
+        assertTrue(inserido)
+    }
+
+    @Test
+    void testExcluirCompetenciaParaCandidato() {
+        Competencia competencia = new Competencia(1, "Java")
+        Candidato candidato = new Candidato(1, null, null, null, null, null, null, null, null, null)
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(candidatoCompetenciaDao.excluirCompetenciaCandidato(Mockito.any(Integer.class), Mockito.any(Integer.class))).thenReturn(true)
+
+        Boolean inserido = candidatoCompetenciaService.excluirCompetenciaDoCandidato(candidato, competencia)
+
+        assertTrue(inserido)
+    }
+}
