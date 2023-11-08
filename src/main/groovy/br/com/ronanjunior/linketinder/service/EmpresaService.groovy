512b3c1a3a7113e8f71b0169e74aff15736774b1
Ronan Vieira do Carmo Juior <ronanvcjunior@gmail.com>
2023-11-08 10:27:36 -0300

    feature: EmpresaService

---
+++ src/main/groovy/br/com/ronanjunior/linketinder/service/EmpresaService.groovy
@@ -0,0 +1,126 @@
+package br.com.ronanjunior.linketinder.service
+
+import br.com.ronanjunior.linketinder.dao.EmpresaDao
+import br.com.ronanjunior.linketinder.model.Empresa
+import br.com.ronanjunior.linketinder.model.Empresa
+import br.com.ronanjunior.linketinder.utils.Conexao
+import br.com.ronanjunior.linketinder.utils.MapperUtils
+
+class EmpresaService {
+    private final Conexao conexao
+    private final MapperUtils mapperUtils
+    private final EmpresaDao empresaDao
+
+    EmpresaService() {
+        this.conexao = new Conexao()
+        this.mapperUtils = new MapperUtils()
+        this.empresaDao = new EmpresaDao(conexao, mapperUtils)
+    }
+
+    EmpresaService(Conexao conexao, MapperUtils mapperUtils, EmpresaDao empresaDao) {
+        this.conexao = conexao
+        this.mapperUtils = mapperUtils
+        this.empresaDao = empresaDao
+    }
+
+    Empresa buscarEmpresaPorId(Empresa empresa) {
+        try {
+            conexao.abrirConexao()
+            return this.montarBuscarEmpresaPorId(empresa.id)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    Empresa buscarEmpresaPorCnpj(Empresa empresa) {
+        try {
+            conexao.abrirConexao()
+            return this.montarBuscarEmpresaPorCnpj(empresa.cnpj)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    Empresa inserirEmpresa(Empresa empresa) {
+        try {
+            conexao.abrirConexao()
+            return this.montarInserirEmpresa(empresa)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    Boolean alterarEmpresa(Empresa empresa) {
+        try {
+            conexao.abrirConexao()
+            return this.montarAlterarEmpresa(empresa)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    Boolean excluirEmpresa(Empresa empresa) {
+        try {
+            conexao.abrirConexao()
+            return this.montarExcluirEmpresa(empresa.id)
+        } catch (Exception e) {
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    protected Empresa montarBuscarEmpresaPorId(Integer idEmpresa) {
+        try {
+            Map empresaMap = empresaDao.buscarEmpresaPorId(idEmpresa)
+            return new Empresa(empresaMap)
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao buscar empresa por id: ${e.message}", e)
+        }
+    }
+
+    protected Empresa montarBuscarEmpresaPorCnpj(String cnpj) {
+        try {
+            Map empresaMap = empresaDao.buscarEmpresaPorCnpj(cnpj)
+            return new Empresa(empresaMap)
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao buscar empresa por cnpj: ${e.message}", e)
+        }
+    }
+
+    protected Empresa montarInserirEmpresa(Empresa empresa) {
+        try {
+            Integer idEmpresa = empresaDao.inserirEmpresa(empresa)
+
+            empresa.setId(idEmpresa)
+
+            return empresa
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao inserir um nova empresa: ${e.message}", e)
+        }
+    }
+
+    protected Boolean montarAlterarEmpresa(Empresa empresa) {
+        try {
+            return empresaDao.atualizarEmpresa(empresa)
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao alterar os dados da empresa: ${e.message}", e)
+        }
+    }
+
+    protected Boolean montarExcluirEmpresa(Integer idEmpresa) {
+        try {
+            return empresaDao.excluirEmpresa(idEmpresa)
+        } catch (Exception e) {
+            throw new Exception("Houve um erro ao excluir empresa: ${e.message}", e)
+        }
+    }
+}
---
+++ src/test/groovy/br/com/ronanjunior/linketinder/service/EmpresaServiceTest.groovy
@@ -0,0 +1,100 @@
+package br.com.ronanjunior.linketinder.service
+
+import br.com.ronanjunior.linketinder.dao.EmpresaDao
+import br.com.ronanjunior.linketinder.model.Empresa
+import br.com.ronanjunior.linketinder.model.Vaga
+import br.com.ronanjunior.linketinder.utils.Conexao
+import br.com.ronanjunior.linketinder.utils.MapperUtils
+import groovy.test.GroovyTestCase
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class EmpresaServiceTest extends GroovyTestCase {
+    @InjectMocks
+    private EmpresaService empresaService
+
+    @Mock
+    private Conexao conexao
+
+    @Mock
+    private EmpresaDao empresaDao
+
+    @Mock
+    private MapperUtils mapperUtils
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    void testBuscarEmpresaPorId() {
+        Empresa retornoEsperado = new Empresa(1, "Empresa", "012345678901234", "Brasil", "12345678", "")
+        Map empresaMap = [id_empresa: 1, nome: "Empresa", cnpj: "012345678901234", pais: "Brasil", cep: "12345678", descricao: ""]
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(empresaDao.buscarEmpresaPorId(Mockito.any(Integer))).thenReturn(empresaMap)
+
+        Empresa retorno = empresaService.buscarEmpresaPorId(retornoEsperado)
+
+        assertEquals(retornoEsperado, retorno)
+    }
+
+    @Test
+    void testBuscarEmpresaPorCnpj() {
+        Empresa retornoEsperado = new Empresa(1, "Empresa", "012345678901234", "Brasil", "12345678", "")
+        Map empresaMap = [id_empresa: 1, nome: "Empresa", cnpj: "012345678901234", pais: "Brasil", cep: "12345678", descricao: ""]
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(empresaDao.buscarEmpresaPorCnpj(Mockito.any(String))).thenReturn(empresaMap)
+
+        Empresa retorno = empresaService.buscarEmpresaPorCnpj(retornoEsperado)
+
+        assertEquals(retornoEsperado, retorno)
+    }
+
+    @Test
+    void testInserirEmpresa() {
+        Empresa empresaEsperado = new Empresa(1, "Empresa", "012345678901234", "Brasil", "12345678", "")
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(empresaDao.inserirEmpresa(Mockito.any(Empresa.class))).thenReturn(1)
+
+        Empresa empresa = empresaService.inserirEmpresa(empresaEsperado)
+
+        assertEquals(empresaEsperado, empresa)
+    }
+
+    @Test
+    void testAlterarEmpresa() {
+        Empresa empresa = new Empresa(1, "Empresa", "012345678901234", "Brasil", "12345678", "")
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(empresaDao.atualizarEmpresa(Mockito.any(Empresa.class))).thenReturn(true)
+
+        Boolean atualizado = empresaService.alterarEmpresa(empresa)
+
+        assertTrue(atualizado)
+    }
+
+    @Test
+    void testExcluirEmpresa() {
+        Empresa empresa = new Empresa(1, "Empresa", "012345678901234", "Brasil", "12345678", "")
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(empresaDao.excluirEmpresa(Mockito.any(Integer.class))).thenReturn(true)
+
+        Boolean excluido = empresaService.excluirEmpresa(empresa)
+
+        assertTrue(excluido)
+    }
+}
