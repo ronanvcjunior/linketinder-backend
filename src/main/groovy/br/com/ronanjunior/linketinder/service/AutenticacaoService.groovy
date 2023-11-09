f9abd066d43376d6c4ecb768c20beb2d02c80bef
Ronan Vieira do Carmo Juior <ronanvcjunior@gmail.com>
2023-11-09 10:30:02 -0300

    feature: AutenticacaoService

---
+++ src/main/groovy/br/com/ronanjunior/linketinder/service/AutenticacaoService.groovy
@@ -0,0 +1,69 @@
+package br.com.ronanjunior.linketinder.service
+
+import br.com.ronanjunior.linketinder.model.Competencia
+import br.com.ronanjunior.linketinder.model.Conta
+import br.com.ronanjunior.linketinder.utils.Conexao
+import br.com.ronanjunior.linketinder.utils.MapperUtils
+
+class AutenticacaoService {
+    private final Conexao conexao
+    private final MapperUtils mapperUtils
+    private final ContaService contaService
+    private final CandidatoService candidatoService
+    private final EmpresaService empresaService
+    private final CandidatoCompetenciaService candidatoCompetenciaService
+
+    AutenticacaoService() {
+        this.conexao = new Conexao()
+        this.mapperUtils = new MapperUtils()
+        this.contaService = new ContaService()
+        this.candidatoService = new CandidatoService()
+        this.empresaService = new EmpresaService()
+        this.candidatoCompetenciaService = new CandidatoCompetenciaService()
+    }
+
+    AutenticacaoService(Conexao conexao, MapperUtils mapperUtils, ContaService contaService, CandidatoService candidatoService, EmpresaService empresaService, CandidatoCompetenciaService candidatoCompetenciaService) {
+        this.conexao = conexao
+        this.mapperUtils = mapperUtils
+        this.contaService = contaService
+        this.candidatoService = candidatoService
+        this.empresaService = empresaService
+        this.candidatoCompetenciaService = candidatoCompetenciaService
+    }
+
+    Conta registrarUsuario(Conta conta) {
+        try {
+            conexao.abrirConexao()
+
+            if (conta.candidato) {
+                conta.candidato.competencias.each { Competencia competencia ->
+                    candidatoCompetenciaService.inserirCompetenciaParaCandidato(conta.candidato, competencia)
+                }
+                conta.candidato = candidatoService.montarInserirCandidato(conta.candidato)
+            }
+
+            if (conta.empresa)
+                conta.empresa = empresaService.montarInserirEmpresa(conta.empresa)
+
+            return  contaService.montarInserirConta(conta)
+        } catch (Exception e) {
+            conexao.rollbackTransacao()
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+
+    Conta Login(Conta conta) {
+        try {
+            conexao.abrirConexao()
+
+            return  contaService.montarBuscarContaPorEmailSenha(conta.email, conta.senha)
+        } catch (Exception e) {
+            conexao.rollbackTransacao()
+            throw new Exception(e.message, e)
+        } finally {
+            conexao.fecharConexao()
+        }
+    }
+}
---
+++ src/test/groovy/br/com/ronanjunior/linketinder/service/AutenticacaoServiceTest.groovy
@@ -0,0 +1,100 @@
+package br.com.ronanjunior.linketinder.service
+
+import br.com.ronanjunior.linketinder.dao.CandidatoDao
+import br.com.ronanjunior.linketinder.model.Candidato
+import br.com.ronanjunior.linketinder.model.Competencia
+import br.com.ronanjunior.linketinder.model.Conta
+import br.com.ronanjunior.linketinder.model.Empresa
+import br.com.ronanjunior.linketinder.model.Vaga
+import br.com.ronanjunior.linketinder.utils.Conexao
+import br.com.ronanjunior.linketinder.utils.MapperUtils
+import groovy.test.GroovyTestCase
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+import java.time.LocalDate
+
+class AutenticacaoServiceTest extends GroovyTestCase {
+    @InjectMocks
+    private AutenticacaoService autenticacaoService
+
+    @Mock
+    private Conexao conexao
+
+    @Mock
+    private ContaService contaService
+
+    @Mock
+    private CandidatoService candidatoService
+
+    @Mock
+    private EmpresaService empresaService
+
+    @Mock
+    private CandidatoCompetenciaService candidatoCompetenciaService
+
+    @Mock
+    private MapperUtils mapperUtils
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    void testInserirUsuarioCandidato() {
+        List<Competencia> competencias = [
+                new Competencia(1, "Java"),
+                new Competencia(2, "Groovy")
+        ]
+        Candidato candidato = new Candidato(1, "Candi", "Dato", "01234567890", LocalDate.of(1970, 1, 1), "Brasil", "GO", "12345678", "", competencias)
+        Conta contaEnvio = new Conta(null, "teste@gmail.com", "teste", candidato, null)
+        Conta contaEsperada = new Conta(1, "teste@gmail.com", "teste", candidato, null)
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(candidatoCompetenciaService.montarInserirCompeteciaParaCandidato(Mockito.any(Integer), Mockito.any(Integer))).thenReturn(true)
+        Mockito.when(candidatoService.montarInserirCandidato(Mockito.any(Candidato))).thenReturn(candidato)
+        Mockito.when(contaService.montarInserirConta(Mockito.any(Conta))).thenReturn(contaEsperada)
+
+        Conta conta = autenticacaoService.registrarUsuario(contaEnvio)
+
+        assertEquals(contaEsperada, conta)
+    }
+
+    @Test
+    void testInserirUsuarioEmpresa() {
+        Empresa empresa = new Empresa(1, "Empresa", "012345678901234", "Brasil", "12345678", "")
+        Conta contaEnvio = new Conta(null, "teste@gmail.com", "teste", null, empresa)
+        Conta contaEsperada = new Conta(1, "teste@gmail.com", "teste", null, empresa)
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(candidatoCompetenciaService.montarInserirCompeteciaParaCandidato(Mockito.any(Integer), Mockito.any(Integer))).thenReturn(true)
+        Mockito.when(empresaService.montarInserirEmpresa(Mockito.any(Empresa))).thenReturn(empresa)
+        Mockito.when(contaService.montarInserirConta(Mockito.any(Conta))).thenReturn(contaEsperada)
+
+        Conta conta = autenticacaoService.registrarUsuario(contaEnvio)
+
+        assertEquals(contaEsperada, conta)
+    }
+
+    @Test
+    void testLogin() {
+        Empresa empresa = new Empresa(1, "Empresa", "012345678901234", "Brasil", "12345678", "")
+        Conta contaEnvio = new Conta(null, "teste@gmail.com", "teste", null, null)
+        Conta contaEsperada = new Conta(1, "teste@gmail.com", "teste", null, empresa)
+
+        Mockito.doNothing().when(conexao).abrirConexao()
+        Mockito.doNothing().when(conexao).fecharConexao()
+        Mockito.when(contaService.montarBuscarContaPorEmailSenha(Mockito.any(String), Mockito.any(String))).thenReturn(contaEsperada)
+
+        Conta conta = autenticacaoService.Login(contaEnvio)
+
+        assertEquals(contaEsperada, conta)
+    }
+}
